ca41370c7c64b7b8da9049db7949b22742ed3c65
3narinder <narinderd9@gmail.com>
2025-12-27 17:12:09 +0530

    Prop types - inline types in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import User from "./components/propTypes/User";
+
 const App = () => {
-  return <div className="flex items-center mt-12 justify-center">App</div>;
+  return (
+    <div className="flex items-center mt-12 justify-center">
+      <User name="John wick" age={20} isStudent={true} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/propTypes/User.tsx
@@ -0,0 +1,19 @@
+const User = ({
+  name,
+  age,
+  isStudent,
+}: {
+  name: string;
+  age: number;
+  isStudent: boolean;
+}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h1>{age}</h1>
+      <h1>{isStudent ? "Student" : "Professional"}</h1>
+    </div>
+  );
+};
+
+export default User;
